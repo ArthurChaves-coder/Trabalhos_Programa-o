a42b435e0073aed70d9ee1388711abba283199f9
ArthurChaves-coder <arthur.chaves@ufn.edu.br>
2026-08-17 14:44:51 -0300

    Update prog7.cpp

--- prog7.cpp
+++ prog7.cpp
@@ -12,8 +12,6 @@ int main(){
     string nome;
     bool houvetroca = false;
     int i;
-    int soma = 0;
-    int quantidadeInseridos = 0;
     string local;
 
     do{
@@ -32,10 +30,10 @@ int main(){
 
         switch(opcao){
             case 1:
-                cout << "digite o nome que quer cadastrar: ";
+                cout << "Digite o nome que quer cadastrar: ";
                 getline(cin, nome);
                 nomes.push_back(nome);
-                cout << "nome cacastrado com sucesso\n";
+                cout << "nome cacastrado com sucesso!\n";
                 break;
             
             case 2:
@@ -43,6 +41,40 @@ int main(){
                 cout << nomes[i] << "\n";
             }
             break;
+            
+            case 3: // limpar lista de nomes
+            nomes.clear();
+            cout << "Vetor limpo!\n";
+            break;
+
+            case 4:
+            cout << "Digite o nome que quer pesquisar: ";
+            getline(cin,nome);
+            for(i = 0; i < nomes.size(); i++){
+                if(nome == nomes[i]){
+                    cout << "Nome encontrado na posicao: " << i << " da lista\n";
+                    break;
+                }
+            }
+            break;
+
+            case 5: // usar tecnica das bolhas
+            houvetroca = true;
+            for(i = 0; i < nomes.size(); i++){
+                    for(int j = 0; j < nomes.size() - 1; j++){
+                        if(nomes[j] > nomes[j + 1]){
+                            local = nomes[j];
+                            nomes[j] = nomes[j + 1];
+                            nomes[j + 1] = local;
+                        }
+                    }
+                    if(!houvetroca){
+                        break;
+                    }
+                    houvetroca = false;
+                }
+                cout << "Nomes ordenados com sucesso!\n";
+                break;
         }
     }while(opcao != 0);
 
